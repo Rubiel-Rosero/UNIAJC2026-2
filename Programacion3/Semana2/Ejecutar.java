5baafa8f5c5c96a00b3c7f564a4ba3bdc4c62677
Rubiel-Rosero <jrrosero@estudiante.uniajc.edu.co>
2026-09-01 14:33:20 +0000

    Creacion de arreglo de manera aleatoria

--- Programacion3/Semana2/Ejecutar.java
+++ Programacion3/Semana2/Ejecutar.java
@@ -7,6 +7,12 @@ public class Ejecutar {
         for (int i = 0; i < a.length; i++) {
             System.out.println("a[" + i + "] = " + a[i]);
         }
+        //Forma 2 - creacion del arreglo de manera aleatoria
+        int[] b = new int[10];
+        for (int i = 0; i < b.length; i++) {
+            b[i] = (int) (Math.random() * 10);
+            System.out.println("b[" + i + "] = " + b[i]);
+        }
         Vector<String> vector = new Vector<>();
         vector.add("Hola, mundo!");
         System.out.println(vector.get(0));
