afcebe83e201e5b8e6498d9cfe8c2014c18d3c86
Rubiel-Rosero <jrrosero@estudiante.uniajc.edu.co>
2026-09-08 09:12:08 -0500

    Matrices

--- Programacion3/Semana3/Matrices.java
+++ Programacion3/Semana3/Matrices.java
@@ -1,5 +1,28 @@
 public class Matrices {
     public static void main(String[] args) {
-        
+        system.out.println("Matrices");
+
+        // Declarar una matriz forma 1
+        int[][] matriz = {{5, 8, 6}, {1, 3, 2}, {4, 9, 7}};
+
+        //recorrer la matriz con un ciclo for anidado
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) { // m[0].length sera siempre para recorrer las columnas
+                System.out.print("m["+""+i+"]["+j+"] = "+matriz[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        //Visualizar de mejor manera la matriz
+        String cad = "";
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) { // m[0].length sera siempre para recorrer las columnas
+                cad += "|"+matriz[i][j];
+            }
+            cad += "|\n";
+        }
+        system.out.println(cad);
+        // Declarar una matriz de textos de 2 filas y 2 columnas
+        String[][] nombres = new String[2][2];
     }
 }
