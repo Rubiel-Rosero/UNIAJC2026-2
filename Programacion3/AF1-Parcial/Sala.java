268c64b76e9f190cae984fd2e77a825264aa81c6
Rubiel-Rosero <jrrosero@estudiante.uniajc.edu.co>
2026-09-25 17:54:44 -0500

    Clase abstracta Sala

---
+++ Programacion3/AF1-Parcial/Sala.java
@@ -0,0 +1,32 @@
+/*
+Esta clase va ser abstracta y va a definir las reglas generales de todas las salas.
+*/
+public abstract class Sala {
+    protected int numero;
+    protected Funcion[] funciones;
+
+    public Sala(int numero){
+        this.numero = numero;
+        this.funciones = new Funcion[3];
+    }
+
+    public int getNumero(){
+        return numero;
+    }
+    
+    public Funcion getFuncion(int franja, Pelicula p){
+        return funciones[franja];
+    }
+
+    public boolean asignarFuncion(int franja, Pelicula p){
+        if(funciones[franja] != null) return false;
+
+        char[][] asientosNuevos = generarMatrizAsientos();
+        funciones[franja] = new Funcion(p, asientosNuevos);
+        return true;
+    }
+
+    public abstract boolean admitePelicula(Pelicula p);
+    public abstract int calcularPrecioSillo(int fila);
+    protected abstract char[][] generarMatrizAsientos();
+}
